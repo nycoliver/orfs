f819d6d4911685015e5f8b1414b832a7c30e2201
ikeafurniture <ikeafurniture@fastmail.com>
2015-10-27 18:45:59 -0400

    Resolve peer and get posts...

--- js/script.js
+++ js/script.js
@@ -1,34 +1,83 @@
 var ipfs_api = require('ipfs-api');
 var ipfs = ipfs_api('localhost', '5001');
+var cjdnsAdmin = require('cjdns-admin');
 var fs = require('fs');
 var id3 = require('id3js');
 var alac = require('alac');
 var restify = require('restify');
 var request = require('request');
-
-var cjdnsAdmin = require('cjdns-admin'),
-    admin,
-    channel;
+var JSONStream = require('JSONStream');
+var es = require('event-stream');
 
 
 var config = require('./config.json');
 
-// create a new Admin
-admin = cjdnsAdmin.createAdmin(config.cjdnsAdmin);
-
-
-
-
 
 var knownPeers = {};
 
+
+function insertPosts(posts) {
+  console.log("adding posts");
+  for (var i = 0; i < posts.length; i++) {
+    console.log(posts[i]);
+    files.files.push(posts[i]);
+  }
+  // Stream.setState({files: files.files});
+}
+
+
+function getPosts(id) {
+  console.log("Resolving ipns " + id);
+
+  ipfs.name.resolve(id, function(err, res) {
+
+    if (err) return console.error(err);
+
+    console.log("Resolved! " + JSON.stringify(res, null, 4))
+    ipfs.ls(res.Path, function(err, res) {
+
+      if(err || !res) return console.error(err)
+
+      console.log("LSd!" + JSON.stringify(res));
+      res.Objects.forEach(function(node) {
+        node.Links.forEach(function(link) {
+          if (link.Name == "files.json")
+            console.log("Found files: " + JSON.stringify(link, null, 4));
+          ipfs.cat(link.Hash, function(err, res) {
+              if(err || !res) return console.error(err)
+
+              if(res.readable) {
+
+                  // Returned as a stream
+                  res.pipe(process.stdout)
+                  res.pipe(JSONStream.parse('files'))
+                  .pipe(es.mapSync(function (data) {
+                    insertPosts(data)
+                  }))
+              } else {
+                  // Returned as a string
+                  insertPosts(res.files);
+              }
+          })
+        })
+      });
+    })
+  })
+}
+
+getPosts("QmXE8vq2LsEmdHHtb95ujTv2Hfag4qTjk2fehwC8HVXcMn");
+
+// create a new Admin
+var admin = cjdnsAdmin.createAdmin(config.cjdnsAdmin);
+
+
 function getInfoOfPeer(peerIp) {
   request("http://["+peerIp+"]:5002/profile", function(err, res, body) {
-    if (err)
-      console.log(err)
-    else {
-      console.log(body);
-    }
+    // if (err)
+    //   // console.log(err)
+    // else {
+    //   // console.log(body);
+    // }
   })
 }
 
@@ -39,7 +88,7 @@ function peersResponse (res) {
     // process ping response
 
 
-    console.dir(JSON.stringify(res.data.routingTable, null, 4));
+    // console.dir(JSON.stringify(res.data.routingTable, null, 4));
 
     for (var i=0; i<res.data.routingTable.length; i++) {
       var peer = res.data.routingTable[i];
@@ -49,7 +98,7 @@ function peersResponse (res) {
       getInfoOfPeer(peer.ip);
     }
 
-    console.log(JSON.stringify(knownPeers, null, 4));
+    // console.log(JSON.stringify(knownPeers, null, 4));
 
     if (res.data && res.data.more)
       dumpPeers(res.args.page+1)
@@ -57,7 +106,7 @@ function peersResponse (res) {
 
 function dumpPeers(page) {
   // ping the admin
-  channel = admin.nodeStore.dumpTable({
+  var channel = admin.nodeStore.dumpTable({
       page: page
   });
 
@@ -79,9 +128,9 @@ window.$ = window.jQuery = require('./js/jquery-2.1.4.min.js');
 
 
 var files;
-var exists = fs.existsSync('./files.json');
+var exists = fs.existsSync('./orfs.json');
 if (exists) {
-  files = require('./files.json');
+  files = require('./orfs.json');
 }
 
 var profile = config.profile;
@@ -95,6 +144,25 @@ function sendProfile(req, res, next) {
 }
 
 server.listen(5002);
+
+
+
+
+
+
+var blobToBase64 = function(blob, cb) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    var dataUrl = reader.result;
+    var base64 = dataUrl.split(',')[1];
+    cb(base64);
+  };
+  reader.readAsDataURL(blob);
+};
+
+
+
+
 
 
 
@@ -128,26 +196,30 @@ var Stream = React.createClass({
     var file = files[0];
     var t = this;
 
-    if (file.type == "audio/mp3") { //or what else?
-      id3({ file: file.path, type: id3.OPEN_LOCAL }, function(err, tags) {
-          // tags now contains your ID3 tags
-          console.log(tags)
-          if (tags.v2.image.data) {
-            var arrayBufferView = new Uint8Array( tags.v2.image.data );
-            var blob = new Blob( [ arrayBufferView ], { type: tags.v2.image.mime } );
-            var urlCreator = window.URL || window.webkitURL;
-            var imageUrl = urlCreator.createObjectURL( blob );
-            console.log(imageUrl)
-
-            blobToBase64(blob, function(base64) {})
-            var data = new Buffer(arrayBufferView, 'base64');
-            add(data);
-
-            metadata['artwork'] = imageUrl
-            console.log(metadata)
-          }
-      });
+    if (file.type == "image/jpeg") {
+      console.log(file);
     }
+
+    // if (file.type == "audio/mp3") { //or what else?
+    //   id3({ file: file.path, type: id3.OPEN_LOCAL }, function(err, tags) {
+    //       // tags now contains your ID3 tags
+    //       console.log(tags)
+    //       if (tags.v2.image.data) {
+    //         var arrayBufferView = new Uint8Array( tags.v2.image.data );
+    //         var blob = new Blob( [ arrayBufferView ], { type: tags.v2.image.mime } );
+    //         var urlCreator = window.URL || window.webkitURL;
+    //         var imageUrl = urlCreator.createObjectURL( blob );
+    //         console.log("Image url: " + imageUrl)
+    //
+    //         blobToBase64(blob, function(base64) {})
+    //         var data = new Buffer(arrayBufferView, 'base64');
+    //         add(data);
+    //
+    //         metadata['artwork'] = imageUrl
+    //         console.log(metadata)
+    //       }
+    //   });
+    // }
 
     function add (data) {
       ipfs.add(data, function (err, res) {
@@ -169,7 +241,7 @@ var Stream = React.createClass({
           files: nextFiles
         })
 
-        fs.writeFile('files.json', JSON.stringify(files))
+        // fs.writeFile('files.json', JSON.stringify(files))
       })
     }
 
